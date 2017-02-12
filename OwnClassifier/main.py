f23baa78ef8bc8fc8c36bebd12164dc141f503cb
PabloPalomar <palomar.pastor@gmail.com>
2017-02-12 13:18:16 +0000

    Added class to build our own KNextNeighbors classifier

--- OwnClassifier/main.py
+++ OwnClassifier/main.py
@@ -1,3 +1,34 @@
+from scipy.spatial import distance
+
+
+def euc(a, b):
+	return distance.euclidean(a, b)
+
+
+class CustomkNNClassifier():
+	def fit(self, X_train, y_train):
+		self.X_train = X_train
+		self.y_train = y_train
+
+	def predict(self, X_test):
+		predictions = []
+		for row in X_test:
+			label = self.closest(row)
+			predictions.append(label)
+
+		return predictions
+
+	def closest(self, row):
+		best_dist = euc(row, self.X_train[0])
+		best_index = 0
+		for i in range(1, len(self.X_train)):
+			dist = euc(row, self.X_train[i])
+			if dist < best_dist:
+				best_dist = dist
+				best_index = i
+		return self.X_train[best_index]
+
+
 from sklearn import datasets
 iris = datasets.load_iris()
 
@@ -7,8 +38,9 @@ y = iris.target
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
 
-from sklearn.neighbors import KNeighborsClassifier
-cls = KNeighborsClassifier()
+# from sklearn.neighbors import KNeighborsClassifier
+# cls = KNeighborsClassifier()
+cls = CustomkNNClassifier()
 
 cls.fit(X_train, y_train)
 predictions = cls.predict(X_test)
